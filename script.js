21e9188776f4d6e55771c3d66e1d2b1d7bff7e48
Henry_DelGlitch <CodingWHenry@gmail.com>
2021-05-07 23:23:44 -0400

    Added my own code

--- script.js
+++ script.js
@@ -3,4 +3,29 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget(){
+  return Math.floor(Math.random * 9);
+}
 
+function compareGuesses(userGuess, computerGuess, number){
+  const humanGuess = Math.abs(number - userGuess);
+  const computerGo = Math.abs(number - computerGuess);
+
+  if(humanGuess < computerGo){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+function updateScore(winner){
+  if(winner === 'human'){
+    humanScore++;
+  }else{
+    computerScore++;
+  }
+}
+
+function advanceRound(){
+  currentRoundNumber++;
+}
